017bce1c01174986b07bfac565120e773fe71e92
Esther Danson <kennedyngo1234@gmail.com>
2025-05-29 15:20:11 +0300

    social links added

--- frontend/src/assets/icons/GetIcons.js
+++ frontend/src/assets/icons/GetIcons.js
@@ -8,6 +8,7 @@ import twitterForLight from './twitter-light.png';
 import telegramForLight from './telegram-light.png';
 import userDark from './user.png';
 import userLight from './user light.png';
-import whatsappicon from './whatsapp.png'
+import whatsappicon from './whatsapp.png';
+import titkokicon from './tiktok.png';
 
-export {facebookForDark, facebookForLight, instagramForDark, instagramForLight, twitterForDark, twitterForLight, telegramForDark, telegramForLight, userDark, userLight, whatsappicon}
+export {facebookForDark, facebookForLight, instagramForDark, instagramForLight, twitterForDark, twitterForLight, telegramForDark, telegramForLight, userDark, userLight, whatsappicon, titkokicon}
