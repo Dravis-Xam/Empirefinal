5bfc1ce86d30a786bd54202db4242c8c2612c8b7
Esther Danson <kennedyngo1234@gmail.com>
2025-05-29 13:51:18 +0300

    gallery fix

--- frontend/src/pages/components/sections/body/DevicesGallery/DevicesGallery.jsx
+++ frontend/src/pages/components/sections/body/DevicesGallery/DevicesGallery.jsx
@@ -8,7 +8,7 @@ export default function DeviceGallery() {
   const [slideIndex, setSlideIndex] = useState(0);
   const containerRef = useRef(null);
   const sliderRef = useRef(null);
-  const cardWidth = 330;
+  const cardWidth = 350;
   const gap = 20;
   const autoScrollInterval = useRef(null);
 
