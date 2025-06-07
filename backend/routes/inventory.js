fa79f5dcd60dc733c67427f5a04dd1d77ca96940
Dravis-Xam <kennedyngo1234@gmail.com>
2025-06-07 18:29:30 +0300

    bug fix

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -43,7 +43,6 @@ router.put(
   "/devices/update/:id",
   authenticateToken,
   authorizeRole('inventory manager'),
-  upload.none(),
   async (req, res) => {
     try {
       const updateData = req.body;
