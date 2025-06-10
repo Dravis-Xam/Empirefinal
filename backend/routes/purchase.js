17954185a5c9a2721135f2e089bd4602b95d708a
Dravis-Xam <kennedyngo1234@gmail.com>
2025-06-10 14:04:05 +0300

    bug fix

--- backend/routes/purchase.js
+++ backend/routes/purchase.js
@@ -5,7 +5,7 @@ import { authenticateToken } from '../middleware/auth.js';
 import { authorizeRole } from '../middleware/authorise.js';
 import { initiateStkPush } from '../utils/stkpush.js';
 import PaymentLog from '../models/paymentLogs.js';
-import pesapal from '../utils/pesapal';
+import pesapal from '../utils/pesapal.js';
 
 const router = express.Router();
 
